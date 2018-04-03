045b47ace300745811c55d64d31e2959e6c56b1b
LSouthey <37827711+LSouthey@users.noreply.github.com>
2018-04-03 11:36:55 +0200

    Initial commit

---
+++ src/FirstWindow.java
@@ -0,0 +1,58 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class FirstWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FirstWindow window = new FirstWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FirstWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnForward = new JButton("Forward");
+		btnForward.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				frame.dispose();
+				new SecondWindow();
+				
+				
+			}
+		});
+		btnForward.setBounds(165, 115, 89, 23);
+		frame.getContentPane().add(btnForward);
+	}
+
+}
